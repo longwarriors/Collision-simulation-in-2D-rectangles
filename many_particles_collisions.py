f7f102b976707f77bc9e63d7f1c064560ccff400
longwarriors <longwarriors@gmail.com>
2023-03-20 00:36:40 +0800

    add collisions between each other

--- many_particles_collisions.py
+++ many_particles_collisions.py
@@ -26,9 +26,9 @@ coords = torch.empty((n_incres + 1, n_particles, 2))
 velocs = torch.empty((n_incres + 1, n_particles, 2))
 
 
-def uniform_points(low_bounds:torch.tensor, high_bounds:torch.tensor, n_points):
+def uniform_points(low_bounds: torch.tensor, high_bounds: torch.tensor, n_points):
     """
-    low_bounds = [x_low, y_low, z_low]
+    low_bounds  = [x_low,  y_low,  z_low]
     high_bounds = [x_high, y_high, z_high]
     在区域内产生均匀分布的点 [[x0, y0, z0], [x1, y1, z1], [x2, y2, z2], ......]
     numpy.random.uniform 的每个维度的边界都一样不能改
@@ -52,6 +52,13 @@ directions = 1 - 2 * torch.rand(n_particles, 2)
 directions = directions / directions.norm(p=2, dim=1).view(-1, 1)
 velocs[0] = speed * directions  # 随机给一个初始速度矢量
 
+"""相互碰撞检测"""
+id_particles = torch.arange(n_particles)  # 粒子编号
+# Pairs of the combined ids 组合编号构成的matrix
+id_pairs = torch.combinations(id_particles, 2)
+n_pairs = len(id_pairs)  # 需要考虑的相互作用数目
+id_pairs_vec = id_pairs.flatten()  # 组合编号构成的vector
+
 
 for n in range(n_incres):
     """碰撞检测
@@ -60,21 +67,57 @@ for n in range(n_incres):
     """
     collisions_vertical = ((coords[n, :, 0] < bound_l + radius) & (velocs[n, :, 0] < 0)) | (
         (coords[n, :, 0] > bound_r - radius) & (velocs[n, :, 0] > 0))
-    collisions_horizon  = ((coords[n, :, 1] < bound_d + radius) & (velocs[n, :, 1] < 0)) | (
+    collisions_horizon = ((coords[n, :, 1] < bound_d + radius) & (velocs[n, :, 1] < 0)) | (
         (coords[n, :, 1] > bound_u - radius) & (velocs[n, :, 1] > 0))
 
     """把撞围栏的粒子速度提取出来"""
     velocs_reflect_vertical = velocs[n][collisions_vertical]
-    velocs_reflect_horizon  = velocs[n][collisions_horizon]
+    velocs_reflect_horizon = velocs[n][collisions_horizon]
 
     """撞竖直围栏则vx反号，撞水平围栏则vy反号"""
-    velocs_reflect_vertical[:, 0] = velocs_reflect_vertical[:, 0] * -1 # -vx
-    velocs_reflect_horizon[:, 1]  = velocs_reflect_horizon[:, 1] * -1 # -vy
+    velocs_reflect_vertical[:, 0] = velocs_reflect_vertical[:, 0] * -1  # -vx
+    velocs_reflect_horizon[:, 1] = velocs_reflect_horizon[:, 1] * -1  # -vy
 
     """把经过修改的速度刷新回去"""
     velocs[n][collisions_vertical] = velocs_reflect_vertical
-    velocs[n][collisions_horizon]  = velocs_reflect_horizon
+    velocs[n][collisions_horizon] = velocs_reflect_horizon
 
+    """相互碰撞检测"""
+    coords_pairs = coords[n].index_select(
+        0, id_pairs_vec)  # 按组合编号vector索引出来的粒子坐标构成的矩阵
+    velocs_pairs = velocs[n].index_select(0, id_pairs_vec)
+    # Pairs of the combined coordinates 二维运动粒子的坐标组合张量
+    coords_pairs = coords_pairs.view(-1, 2, 2)
+    velocs_pairs = velocs_pairs.view(-1, 2, 2)
+
+    dx_dy = coords_pairs.diff(dim=1)  # 坐标组合张量第二个维度做差得到 [x_i - x_j, y_i - y_j]
+    dx_dy.squeeze_()  # Relative position of the combined coordinates 压缩所有值为1的维度成为matrix
+    # Distances of the combined coordinates 粒子的坐标组合的距离vector
+    distance_pairs = dx_dy.norm(dim=-1)
+    collisions_other = distance_pairs < 2 * radius  # 此刻相互距离小于2r的小球组合在下一刻被弹开
+
+    """把相互碰撞的粒子坐标和速度提取出来"""
+    coords_collided_pairs = coords_pairs[collisions_other]  # 碰撞组合的坐标
+    velocs_collided_pairs = velocs_pairs[collisions_other]  # 碰撞组合的速度
+    dr_collided_pairs = coords_collided_pairs.diff(
+        dim=1).squeeze()  # 碰撞组合的相对位置
+    dv_collided_pairs = velocs_collided_pairs.diff(
+        dim=1).squeeze()  # 碰撞组合的相对速度
+
+    """相互碰撞后的粒子速度"""
+    k_collided_pairs = dr_collided_pairs * dv_collided_pairs
+    k_collided_pairs = k_collided_pairs.sum(dim=1)  # 内积
+    k_collided_pairs = k_collided_pairs / distance_pairs[collisions_other]**2
+    k_collided_pairs = torch.einsum(
+        "i,ij->ij", k_collided_pairs, dr_collided_pairs)
+
+    velocs_collided_pairs[:,
+                          0] = velocs_collided_pairs[:, 0] + k_collided_pairs
+    velocs_collided_pairs[:,
+                          1] = velocs_collided_pairs[:, 1] - k_collided_pairs
+    velocs_new = velocs_collided_pairs.flatten(0, 1)  # list of vectors
+    id_collided = id_pairs[collisions].flatten()  # 碰撞粒子组合的编号
+    velocs[n].index_copy_(0, id_collided, velocs_new)  # 按碰撞的粒子编号替换新速度矢量
 
     """按新速度更新粒子位置"""
     coords[n + 1] = coords[n] + velocs[n] * dt
@@ -108,10 +151,10 @@ timer = ax1.text(0.8, 0.5, "", fontsize=15,
 
 # 创建画布的图线 creating empty plots
 lines = []
-dots  = []
+dots = []
 for _ in range(n_particles):
     line, = ax1.plot([], [], linewidth=3, color="cornflowerblue")
-    dot,  = ax1.plot([], [], 'yo', markersize=10, markeredgecolor='r')
+    dot, = ax1.plot([], [], 'yo', markersize=10, markeredgecolor='r')
     lines.append(line)
     dots.append(dot)
 
@@ -122,7 +165,7 @@ def init():
     for i in range(n_particles):
         lines[i].set_data([], [])
         dots[i].set_data([], [])
-    return timer, *lines, *dots # 解包很重要！
+    return timer, *lines, *dots  # 解包很重要！
 
 
 # 更新新一帧的数据 refresh
@@ -131,7 +174,7 @@ def update(n):
     for i in range(n_particles):
         lines[i].set_data(coords[:n, i, 0], coords[:n, i, 1])
         dots[i].set_data(coords[n, i, 0], coords[n, i, 1])
-    return timer, *lines, *dots # 解包很重要！
+    return timer, *lines, *dots  # 解包很重要！
 
 
 # perform animation
