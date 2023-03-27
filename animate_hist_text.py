1ecf3f81beacc90f1153ef4002284c7622ce1af8
longwarriors <longwarriors@gmail.com>
2023-03-27 14:35:35 +0800

    succeed in animating ax.text() with ax.hsit() when setting blit=False

--- animate_hist_text.py
+++ animate_hist_text.py
@@ -12,27 +12,19 @@ plt.style.use(["science", "notebook", "grid"])
 ts = 200
 data = np.random.randn(ts, 20000)
 n_segment = 88  # n_interval 统计的区间数
-bin_edges = np.linspace(-4, 4, n_segment + 1)  # edges or vertices
-
-
-def prepare_animation(bar_container, timer):
-
-    def animate(frame_number):
-        timer.set_text("time = {:.3f}".format(frame_number))
-        hist, _ = np.histogram(
-            data_velocs[n], bin_edges, density=True)  # _ = bin_edges
-        for count, rect in zip(n, bar_container.patches):
-            rect.set_height(count)
-        return timer, bar_container.patches
-
-    return animate
+bin_edges = np.linspace(-5, 5, n_segment + 1)  # edges or vertices
 
 
 # 创建画板
 fig = plt.figure(figsize=(8, 4))
 fig.set_tight_layout(True)  # 紧凑组排
 fig.text(
-    x=0.8, y=0.04, s="Designed by longwarriors", style="italic", fontsize=8, color="red"
+    x=0.8, 
+    y=0.04, 
+    s="Designed by longwarriors", 
+    style="italic", 
+    fontsize=8, 
+    color="red"
 )
 
 
@@ -40,7 +32,12 @@ fig.text(
 ax1 = plt.subplot(1, 1, 1)
 ax1.set_ylim(top=1.0)
 _, _, bar_container = ax1.hist(
-    data[0], bin_edges, lw=1, ec="yellow", fc="green", alpha=0.5
+    [], 
+    bin_edges, 
+    lw=1, 
+    ec="yellow", 
+    fc="green", 
+    alpha=0.5
 )
 timer = ax1.text(
     0.8,
@@ -52,6 +49,32 @@ timer = ax1.text(
 )  # text实例
 
 
+# initialize 清空当前帧
+def init():
+    timer.set_text("")
+    hist, _ = np.histogram(np.zeros_like(data[0]), bin_edges, density=True)  # _ = bin_edges
+    for count, rect in zip(hist, bar_container.patches):
+        rect.set_height(count)
+    return timer, bar_container.patches
+
+
+# refresh the present frame 更新新一帧的数据 
+def update(frame_number):
+    timer.set_text("time = {:.3f}".format(frame_number))
+    hist, _ = np.histogram(data[frame_number], bin_edges, density=True)  # _ = bin_edges
+    for count, rect in zip(hist, bar_container.patches):
+        rect.set_height(count)
+    return timer, bar_container.patches
+
+
 ani = animation.FuncAnimation(
-    fig, prepare_animation, 50, repeat=False, blit=True)
+    fig,
+    update,
+    frames=np.arange(0, ts, 1, dtype=int),
+    interval=0,  # 帧之间的延迟（毫秒）默认为200
+    blit=False,  # 是否执行blitting优化
+    init_func=init,
+)
+# ani = animation.FuncAnimation(fig, update, 1, repeat=False, blit=False)
+
 plt.show()
